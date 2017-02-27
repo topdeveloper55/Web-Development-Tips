618eb9ad14aad57448bf952a4357d21970862a3f
joaqperalta <lahim.kane@outlook.com>
2017-02-27 17:30:15 +0000

    Update loading screen

--- src/app.js
+++ src/app.js
@@ -4,5 +4,7 @@ import AppRoutes from './components/AppRoutes.jsx';
 require('./static/css/base.scss');
 require('./static/js/prism.js');
 window.onload = () => {
+  var loader = document.querySelector('.loader');
+  loader.className += " loaderDone";
   ReactDOM.render(<AppRoutes/>,document.getElementById('container'));
 }
